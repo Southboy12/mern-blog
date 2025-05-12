4bdc2435beec64bff928e75c4bc8feb6ab05ddb8
Southboy12 <menkachi85@gmail.comgit config --global user.email menkachi85@gmail.comgit config --global user.email menkachi85@gmail.com>
2025-05-12 13:50:31 +0100

    Initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>About</h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia eaque perferendis cupiditate quisquam, deleniti fugiat. Corporis iusto, est molestias doloremque magnam nostrum dolorum voluptatum quae optio expedita, aspernatur sit mollitia suscipit? Ipsa optio earum voluptatibus quia, sunt magnam praesentium blanditiis accusantium libero ipsam fuga exercitationem illum mollitia officia vero ullam!</p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia eaque perferendis cupiditate quisquam, deleniti fugiat. Corporis iusto, est molestias doloremque magnam nostrum dolorum voluptatum quae optio expedita, aspernatur sit mollitia suscipit? Ipsa optio earum voluptatibus quia, sunt magnam praesentium blanditiis accusantium libero ipsam fuga exercitationem illum mollitia officia vero ullam!</p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia eaque perferendis cupiditate quisquam, deleniti fugiat. Corporis iusto, est molestias doloremque magnam nostrum dolorum voluptatum quae optio expedita, aspernatur sit mollitia suscipit? Ipsa optio earum voluptatibus quia, sunt magnam praesentium blanditiis accusantium libero ipsam fuga exercitationem illum mollitia officia vero ullam!</p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque vero quisquam numquam architecto atque vel nulla facere nam, laudantium, expedita fugiat et ipsa quam culpa mollitia, omnis at sequi minima. Nam quisquam error temporibus soluta sunt dignissimos quia ducimus rerum officiis excepturi, quod labore eveniet nemo accusamus eos minus dolores sit at voluptatibus ea repellendus assumenda ex recusandae quas? Reiciendis placeat voluptatum sapiente, dolorum officia totam. Consequuntur et reiciendis magni aliquid sit ut odio mollitia dicta eligendi cupiditate? Autem expedita totam earum, libero reprehenderit necessitatibus deserunt dolores perferendis mollitia unde, laboriosam ab quia cum accusantium enim error deleniti labore! Illum.</p>
+    </div>
+  )
+}
+
+export default About
